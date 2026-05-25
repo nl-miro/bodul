5927af9a85021872a2f1b81ee101598ac667be35
Miro <nulllabsmiro@gmail.com>
2026-05-25 10:23:06 +0200

    Move RetailerCode enum to retailer-guild lib
    The MinisForum storefront identifier doesn't belong inside the
    sourcing service binary; promote it to the shared retailer-guild lib
    so other services can import it via retailer_guild::io.

--- lib/retailer-guild/src/lib.rs
+++ lib/retailer-guild/src/lib.rs
@@ -1,1 +1,42 @@
+pub mod io {
+    pub use crate::models::RetailerCode;
+}
 
+mod models {
+    #[allow(non_camel_case_types)]
+    pub enum RetailerCode {
+        MINISFORUM_EU,
+        MINISFORUM_US,
+        MINISFORUM_UK,
+        MINISFORUM_FR,
+        MINISFORUM_CA,
+        MINISFORUM_AU,
+    }
+
+    impl RetailerCode {
+        pub fn as_string(&self) -> &'static str {
+            match self {
+                RetailerCode::MINISFORUM_EU => "MinisForumEU",
+                RetailerCode::MINISFORUM_US => "MinisForumUS",
+                RetailerCode::MINISFORUM_UK => "MinisForumUK",
+                RetailerCode::MINISFORUM_FR => "MinisForumFR",
+                RetailerCode::MINISFORUM_CA => "MinisForumCA",
+                RetailerCode::MINISFORUM_AU => "MinisForumAU",
+            }
+        }
+    }
+
+    impl From<String> for RetailerCode {
+        fn from(value: String) -> Self {
+            match value.as_str() {
+                "MinisForumEU" => RetailerCode::MINISFORUM_EU,
+                "MinisForumUS" => RetailerCode::MINISFORUM_US,
+                "MinisForumUK" => RetailerCode::MINISFORUM_UK,
+                "MinisForumFR" => RetailerCode::MINISFORUM_FR,
+                "MinisForumCA" => RetailerCode::MINISFORUM_CA,
+                "MinisForumAU" => RetailerCode::MINISFORUM_AU,
+                other => panic!("unknown RetailerCode: {other}"),
+            }
+        }
+    }
+}
